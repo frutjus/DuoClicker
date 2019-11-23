46694e7bbcf8ec30db9942997caac1c3fe6cd248
Frutjus <frutjus@gmail.com>
2019-11-23 07:40:47 +0200

    added zero functionality

--- background.js
+++ background.js
@@ -3,10 +3,15 @@ chrome.runtime.onInstalled.addListener(function() {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {urlEquals: 'https://orteil.dashnet.org/cookieclicker/'}
+				pageUrl: {urlContains: 'http' /*urlEquals: 'https://orteil.dashnet.org/cookieclicker/'*/}
 			})],
 			actions: [new chrome.declarativeContent.ShowPageAction()]
 		}]);
 	});
 });
 
+chrome.pageAction.onClicked.addListener(function (tab) {
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'inject.js'
+	});
+});
